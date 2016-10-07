11bfaa8b720b6f8e546e40b676e9fd5a1b7a17aa
Matt Carter <m@ttcarter.com>
2016-10-07 10:42:52 +1000

    BUGFIX: Better handling of weird values for current/max when drawing the bar

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ function CLIProgress(text, settings) {
 		// Rendering a bar? {{{
 		if (text.indexOf('[[BAR') > -1) {
 			var maxBarWidth = windowSize.width - stringLength(text);
-			var barCompleteWidth = Math.round(Math.min(progress.settings.current, progress.settings.max) / progress.settings.max * maxBarWidth);
+			if (!progress.settings.max) progress.settings.max = progress.settings.current > 0 ? progress.settings.current : 0;
+			var barCompleteWidth = Math.round(Math.min(progress.settings.current, progress.settings.max) / (progress.settings.max * maxBarWidth));
 
 			var completeBits = Array(barCompleteWidth).join(progress.settings.completeChar);
 			var incompleteBits = maxBarWidth - barCompleteWidth > 0 ? Array(maxBarWidth - barCompleteWidth).join(progress.settings.incompleteChar) : '';
